146026345d742705c12271ee87740fbe6ec17726
esslamb <amelia1223.ess@gmail.com>
2022-01-20 05:14:53 +0800

    given up

--- collect.py
+++ collect.py
@@ -3,6 +3,9 @@ from asyncio.windows_events import NULL
 import yfinance as yahooFinance
 import datetime
 
+import numpy as np
+from scipy.linalg import solve
+
 def adj_close(stock):
     amazon=yahooFinance.Ticker(stock)
     startDate= datetime.datetime(2017,1,1)
@@ -13,9 +16,8 @@ def adj_close(stock):
     return adj_close
 
 adj_close1=adj_close('AMZN')
-
 #print(adj_close)
-
+adj_close2=adj_close1.iloc[1:,:]
 
 #break the data into 30 day intervals\
 def breaker(interval, data):
@@ -23,6 +25,7 @@ def breaker(interval, data):
     for i in range(len(data)):
         if(i+1)% interval == 0:
             breaker.append(data[(i-interval+1):i+1])
+    del breaker[len(breaker)-1] #remove the last interval
     return breaker
 
 
@@ -31,10 +34,19 @@ def topredict(interval, data):
     for i in range(len(data)):
         if(i+1)% interval == 0:
             topredict.append(data[i-interval+1:i-interval+2])
-            #beware the second entry ([1]) is the first to be predicted
+    del topredict[0] #remove the 1st entry
     return topredict
 
-b=breaker(5, adj_close1)
-t=topredict(5, adj_close1)
-print(b[0])
-print(t[0])
+b1= breaker(5, adj_close1) #start at entry 0
+Y1= breaker(5, adj_close2) #start at entry 1
+tpredict = topredict(5, adj_close2)
+#print(b1[0], Y1[0])
+#print(tpredict[0])
+
+
+Y=Y1[0].to_numpy()
+b=b1[0].to_numpy()
+print(Y)
+print(b)
+X=solve(b, Y)
+print(X)
--- qr.py
+++ qr.py
@@ -1,6 +1,37 @@
 import numpy as np
 from scipy.linalg import qr
 
+from numpy import array
+from numpy.linalg import inv
+from numpy.linalg import qr
+from matplotlib import pyplot
+
+data = array([
+[0.05, 0.12],
+[0.18, 0.22],
+[0.31, 0.35],
+[0.42, 0.38],
+[0.5, 0.49],
+])
+from collect import Y1, b1, tpredict
+
+Y=np.array(Y1[0])
+b=np.array(b1[0])
+X=np.linalg.solve(b, Y)
+print(X)
+# QR decomposition
+Q, R = qr(X)
+b = inv(R).dot(Q.T).dot(y)
+print(b)
+# predict using coefficients
+yhat = X.dot(b)
+
+# plot data and predictions
+def plot(x,y):
+    pyplot.scatter(X, y)
+    pyplot.plot(X, yhat, color='red')
+    pyplot.show()
+
 def QR_Decomposition(A):
     n, m = A.shape # get the shape of A
     Q = np.empty((n, n)) # initialize matrix Q
@@ -17,4 +48,3 @@ def QR_Decomposition(A):
         for j in range(i, m):
             R[i, j] = A[:, j] @ Q[:, i]
     return Q, R
-
